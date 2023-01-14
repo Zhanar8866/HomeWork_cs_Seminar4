dd0e6a29ed579b7829ebefcb886206306fe3ce39
Zhanar S <zhan-ct@yandex.ru>
2023-01-14 18:19:40 +0300

    добавлено решение по Задаче27 - сумма цифр в числе

---
+++ DZ_02_Sum/Program.cs
@@ -0,0 +1,19 @@
+﻿/* Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12
+*/
+
+Console.Write("Введите число: ");
+int A = Convert.ToInt32(Console.ReadLine());
+
+int sum = 0;
+
+for (int i = 1; A > 0; i++)
+{
+    int ost = A % 10;
+    sum = sum + ost;
+    A = A / 10;
+}
+
+Console.WriteLine(sum);
